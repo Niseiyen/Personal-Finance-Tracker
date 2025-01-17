f2185b90ef2d4918f2db5433ba231536a0ba4150
Niseiyen <127323783+Niseiyen@users.noreply.github.com>
2025-01-16 21:10:28 -0500

    Correction bug: Les design cachaient les autres boutons + ajout d'animaiton pour l'entree et sortir du dropdown

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,12 +4,12 @@ import { TrashIcon, Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import { useTranslation } from "react-i18next";
 import i18next from "i18next";
 
-const Nav = ({ userName}) => {
+const Nav = ({ userName }) => {
     const [menuOpen, setMenuOpen] = useState(false);
 
     const changeLanguage = (lng) => {
         i18next.changeLanguage(lng);
-        localStorage.setItem('language', lng); 
+        localStorage.setItem('language', lng);
     };
 
     const { t } = useTranslation();
@@ -25,14 +25,14 @@ const Nav = ({ userName}) => {
             <div className="hidden md:flex items-center space-x-4">
                 <button
                     onClick={() => changeLanguage("en")}
-                    className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
+                    className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
                 >
                     <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
                     <span className="relative" style={{ top: '2px', left: '2px' }}>English</span>
                 </button>
                 <button
                     onClick={() => changeLanguage("fr")}
-                    className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
+                    className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
                 >
                     <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
                     <span className="relative" style={{ top: '2px', left: '2px' }}>Français</span>
@@ -50,7 +50,7 @@ const Nav = ({ userName}) => {
                     >
                         <button
                             type="submit"
-                            className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
+                            className="relative flex items-center py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
                         >
                             <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
                             <span className="relative flex items-center" style={{ top: '2px', left: '2px' }}>
@@ -73,47 +73,50 @@ const Nav = ({ userName}) => {
                 )}
             </button>
 
-            {menuOpen && (
-                <div className="absolute top-16 left-0 w-full bg-gray-800 z-10 p-4 space-y-4 md:hidden">
-                    <button
-                        onClick={() => changeLanguage("en")}
-                        className="block w-full py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
-                    >
-                        <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
-                        <span className="relative" style={{ top: '2px', left: '2px' }}>English</span>
-                    </button>
-                    <button
-                        onClick={() => changeLanguage("fr")}
-                        className="block w-full py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
-                    >
-                        <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
-                        <span className="relative" style={{ top: '2px', left: '2px' }}>Français</span>
-                    </button>
+            {/* Dropdown avec animation */}
+            <div
+                className={`absolute top-12 left-0 w-full bg-gray-800 z-10 p-4 p-8 space-y-4 md:hidden transition-all duration-300 ease-in-out transform ${
+                    menuOpen ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"
+                }`}
+            >
+                <button
+                    onClick={() => changeLanguage("en")}
+                    className="block w-full ps-2 py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
+                >
+                    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
+                    <span className="relative" style={{ top: '2px', left: '2px' }}>English</span>
+                </button>
+                <button
+                    onClick={() => changeLanguage("fr")}
+                    className="block w-full py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
+                >
+                    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
+                    <span className="relative" style={{ top: '2px', left: '2px' }}>Français</span>
+                </button>
 
-                    {userName && (
-                        <Form
-                            method="post"
-                            action="logout"
-                            onSubmit={(event) => {
-                                if (!confirm("Delete user and all data?")) {
-                                    event.preventDefault();
-                                }
-                            }}
-                            className="block"
+                {userName && (
+                    <Form
+                        method="post"
+                        action="logout"
+                        onSubmit={(event) => {
+                            if (!confirm("Delete user and all data?")) {
+                                event.preventDefault();
+                            }
+                        }}
+                        className="block"
+                    >
+                        <button
+                            type="submit"
+                            className="block w-full py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition-all duration-300 transform hover:scale-105 hover:shadow-lg hover:translate-y-1"
                         >
-                            <button
-                                type="submit"
-                                className="block w-full py-2 px-4 bg-gray-700 text-white font-bold rounded-md transition duration-200 hover:bg-gray-600 hover:text-yellow-400"
-                            >
-                                <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
-                                <span className="relative flex items-center justify-center" style={{ top: '2px', left: '2px' }}>
-                                    Delete User <TrashIcon width={20} className="ml-2" />
-                                </span>
-                            </button>
-                        </Form>
-                    )}
-                </div>
-            )}
+                            <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-gray-900"></span>
+                            <span className="relative flex items-center justify-center" style={{ top: '2px', left: '2px' }}>
+                                {t("deleteUser")} <TrashIcon width={20} className="ml-2" />
+                            </span>
+                        </button>
+                    </Form>
+                )}
+            </div>
         </nav>
     );
 };
